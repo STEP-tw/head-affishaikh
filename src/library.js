966efdc205fcd84405883f32d9f36aa251c61c36
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-04 15:21:13 +0530

    Modified the function sliceContents so that it return the required number of lines.

--- src/library.js
+++ src/library.js
@@ -1,5 +1,9 @@
-const sliceContents = function(fs, filePath) {
-  let result = fs.readFileSync(filePath, 'utf8');
+const sliceContents = function(fs, filePath, numberOfLines) {
+  if(numberOfLines === undefined) {
+    numberOfLines = 10;
+  }
+
+  let result = fs.readFileSync(filePath, 'utf8').split('\n').slice(0,numberOfLines).join('\n');
   return result;
 }
 
--- test/libTest.js
+++ test/libTest.js
@@ -7,13 +7,20 @@ const readFileSync = function(filePath, encoding) {
 
 const fs = {
   './testData/testFile1' : 'Hello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello',
+
+  './testData/testFile2' : 'Optimus Prime\nBumble Bee\nBulkhead\nArcee\nRatchet\nWheeljack\nUltramagnus\nSmoke Scream\nJack\nMico\nRaff',
   readFileSync : readFileSync
 }
 
 describe('sliceContents', function() {
   it('should return a string of ten lines when given reader object and filename', function() {
-    let expectedOutput = fs.readFileSync('./testData/testFile1','utf8');
+    let expectedOutput = 'Hello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello'; 
     let actualOutput = sliceContents(fs, './testData/testFile1');
+    assert.equal(actualOutput, expectedOutput);
+  });
+  it('should return a string of two lines when given reader object, filepath and 2 numberOfLines', function() {
+    let expectedOutput = 'Optimus Prime\nBumble Bee'; 
+    let actualOutput = sliceContents(fs, './testData/testFile2', 2);
     assert.equal(actualOutput, expectedOutput);
   });
 });
