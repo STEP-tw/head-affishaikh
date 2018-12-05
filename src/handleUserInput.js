6bef0112cb2031c173c7b335ad116945c56d364a
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-05 21:45:37 +0530

    Modified the function handleUserInput to work it with -c.

--- src/handleUserInput.js
+++ src/handleUserInput.js
@@ -1,26 +1,32 @@
+const {optionExtractor} = require('./util.js');
+
 const extractUserInput = function(userInputForHead) {
   let headPrerequisites = {};
   headPrerequisites.filePaths = [];
   let substrIndex = 1;
-  let numberOfLines = 10;
+  let optionValue = 10;
   let fileReaderIndex = 0;
+  let option = optionExtractor(userInputForHead[0]);
+  let regex = '^'+option+'[0-9]';
+  regex = new RegExp(regex);
 
-  if(userInputForHead[0].match(/^-n[0-9]/)) {
+  if(userInputForHead[0].match(regex)) {
     substrIndex = 2;
   }
 
   if(userInputForHead[0].match(/^-/)) {
-    numberOfLines = +userInputForHead[0].substr(substrIndex);
+    optionValue = +userInputForHead[0].substr(substrIndex);
     fileReaderIndex = 1;
   }
   
-  if(userInputForHead[0] === '-n') {
-    numberOfLines = +userInputForHead[1];
+  if(userInputForHead[0] === option) {
+    optionValue = +userInputForHead[1];
     fileReaderIndex = 2;
   }
   
-  headPrerequisites.numberOfLines = numberOfLines;
+  headPrerequisites.optionValue = optionValue;
   headPrerequisites.filePaths = userInputForHead.slice(fileReaderIndex);
+  headPrerequisites.option = option;
   return headPrerequisites;
 }
 
