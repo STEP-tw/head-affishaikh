95b6f37242b612255f1732732fcae2f5e036ac47
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-05 09:46:03 +0530

    Modified the function and tests to pass the arguments in object.

--- src/library.js
+++ src/library.js
@@ -1,7 +1,19 @@
 const sliceContents = function(fs, dataForSlicing) {
-  let filePath = dataForSlicing.fileNames[0]
+  let numberOfFiles = dataForSlicing.filePaths.length;
   let numberOfLines = dataForSlicing.numberOfLines;
-  let result = fs.readFileSync(filePath, 'utf8').split('\n').slice(0,numberOfLines).join('\n');
+  let filePaths = dataForSlicing.filePaths;
+  let delimeter = '';
+  let result = '';
+  if(numberOfFiles === 1) {
+    result = result + fs.readFileSync(filePaths[0], 'utf8').split('\n').slice(0,numberOfLines).join('\n');
+    return result;
+  }
+
+  for(let filePath of filePaths) {
+    result = result + delimeter + '==> '+ filePath + ' <==\n';
+    result = result + fs.readFileSync(filePath, 'utf8').split('\n').slice(0,numberOfLines).join('\n');
+    delimeter = '\n\n';
+  }
   return result;
 }
 
--- test/libTest.js
+++ test/libTest.js
@@ -15,22 +15,24 @@ const fs = {
 
 describe('sliceContents', function() {
   it('should return a string of ten lines when given reader object and filename', function() {
-    let dataForSlicing = {fileNames : ['./testData/testFile1'], numberOfLines: 10}
+    let dataForSlicing = {filePaths : ['./testData/testFile1'], numberOfLines: 10}
     let expectedOutput = 'Hello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello'; 
     let actualOutput = sliceContents(fs, dataForSlicing);
     assert.equal(actualOutput, expectedOutput);
   });
 
   it('should return a string of two lines when given reader object, filepath and 2 numberOfLines', function() {
+    let dataForSlicing = {filePaths : ['./testData/testFile2'], numberOfLines: 2}
     let expectedOutput = 'Optimus Prime\nBumble Bee'; 
-    let actualOutput = sliceContents(fs, './testData/testFile2', 2);
+    let actualOutput = sliceContents(fs, dataForSlicing);
     assert.equal(actualOutput, expectedOutput);
   });
 
   it('should return a string of 10 lines from testFile1 concatinatd with string of 10 lines from testFile2', function() {
-    let expectedOutput = '==> ./testData/testFile1 <==\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\n\n==> ./testData/testFile2 <==\nOptimus Prime\nBumble Bee\nBulkhead\nArcee\nRatchet\nWheeljack\nUltramagnus\nSmoke Scream\nJack\nMico\nRaff';
+    let dataForSlicing = {filePaths : ['./testData/testFile1', './testData/testFile2'], numberOfLines: 10}
+    let expectedOutput = '==> ./testData/testFile1 <==\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\n\n==> ./testData/testFile2 <==\nOptimus Prime\nBumble Bee\nBulkhead\nArcee\nRatchet\nWheeljack\nUltramagnus\nSmoke Scream\nJack\nMico';
 
-    let actualOutput = sliceContents(fs, './testData/testFile1', './testData/testFile2');
+    let actualOutput = sliceContents(fs, dataForSlicing);
 
     assert.equal(actualOutput, expectedOutput);
   });
