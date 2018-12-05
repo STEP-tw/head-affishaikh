0887c9b48eac2c9f40f33e989b617852bdf0794f
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-05 12:18:24 +0530

    Added tests for the function extractUserInput.

--- src/handleUserInput.js
+++ src/handleUserInput.js
@@ -8,17 +8,19 @@ const extractUserInput = function(userInputForHead) {
   if(userInputForHead[0].match(/^-n[0-9]/)) {
     substrIndex = 2;
     fileReaderIndex = 1;
+    numberOfLines = +userInputForHead[0].substr(substrIndex,);
   }
 
-  if(userInputForHead[0].match(/^-/)) {
+  if(userInputForHead[0].match(/^-[0-9]/)) {
     numberOfLines = +userInputForHead[0].substr(substrIndex,);
+    fileReaderIndex = 1;
   }
   
   dataForSlicing.numberOfLines = numberOfLines;
 
   if(userInputForHead[0] === '-n') {
     dataForSlicing.numberOfLines = +userInputForHead[1];
-    fileReaderIndex = 3;
+    fileReaderIndex = 2;
   }
   
   for(; fileReaderIndex <= userInputForHead.length-1; fileReaderIndex++) {
--- test/handleUserInputTest.js
+++ test/handleUserInputTest.js
@@ -13,4 +13,28 @@ describe('extractUserInput', function() {
   let actualOutput = extractUserInput(['-n5','file1']);
   assert.deepEqual(expectedOutput, actualOutput);
   });
+
+  it('should return an object of 2 filepath and 5 numberOfLines when given two files and -n5', function() {
+  let expectedOutput = {'filePaths' : ['file1','file2'], 'numberOfLines' : 5}; 
+  let actualOutput = extractUserInput(['-n5','file1','file2']);
+  assert.deepEqual(expectedOutput, actualOutput);
+  });
+
+  it('should return an object of 2 filepath and 5 numberOfLines when given two files and -n 5', function() {
+  let expectedOutput = {'filePaths' : ['file1','file2'], 'numberOfLines' : 5}; 
+  let actualOutput = extractUserInput(['-n', '5','file1','file2']);
+  assert.deepEqual(expectedOutput, actualOutput);
+  });
+
+  it('should return an object of 2 filepaths and 5 numberOfLines when given two files and -5', function() {
+  let expectedOutput = {'filePaths' : ['file1','file2'], 'numberOfLines' : 5}; 
+  let actualOutput = extractUserInput(['-5','file1','file2']);
+  assert.deepEqual(expectedOutput, actualOutput);
+  });
+
+  it('should return an object of 2 filepaths and 10 numberOfLines when given two files', function() {
+  let expectedOutput = {'filePaths' : ['file1','file2'], 'numberOfLines' : 5}; 
+  let actualOutput = extractUserInput(['-5','file1','file2']);
+  assert.deepEqual(expectedOutput, actualOutput);
+  });
 });
