a50bae0c8072bc6fa99dbc985fcc25787f1194d7
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-05 11:51:57 +0530

    Removed the only from tests.

--- test/handleUserInputTest.js
+++ test/handleUserInputTest.js
@@ -2,13 +2,13 @@ const {extractUserInput} = require('../src/handleUserInput.js');
 const assert = require('assert');
 
 describe('extractUserInput', function() {
-  it.only('should return an object of 1 filepath and 10 numberOfLines when given only one file', function() {
+  it('should return an object of 1 filepath and 10 numberOfLines when given only one file', function() {
   let expectedOutput = {'filePaths' : ['file1'], 'numberOfLines' : 10}; 
   let actualOutput = extractUserInput(['file1']);
   assert.deepEqual(expectedOutput, actualOutput);
   });
 
-  it.only('should return an object of 1 filepath and 5 numberOfLines when given only one file and -n5', function() {
+  it('should return an object of 1 filepath and 5 numberOfLines when given only one file and -n5', function() {
   let expectedOutput = {'filePaths' : ['file1'], 'numberOfLines' : 5}; 
   let actualOutput = extractUserInput(['-n5','file1']);
   assert.deepEqual(expectedOutput, actualOutput);
