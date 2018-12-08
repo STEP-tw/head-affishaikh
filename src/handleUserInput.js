8850c39eaacffe3bc6454a16a9484ada4625fef0
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-08 12:18:17 +0530

    Refactored the handleUserInput.js
    Changed variable names.

--- src/handleUserInput.js
+++ src/handleUserInput.js
@@ -1,35 +1,35 @@
 const { optionExtractor } = require("./util.js");
 
-const isOptionAndValueAttached = (option, regex) => option.match(regex);
+const isOptionExplicit = (option, regex) => option.match(regex);
 
 const getSubstrIndex = function(option, regex) {
   let substrIndex = 1;
-  if (isOptionAndValueAttached(option, regex)) {
+  if (isOptionExplicit(option, regex)) {
     substrIndex = 2;
   }
   return substrIndex;
 };
 
 const isOptionValueGiven = option => option.match(/^-/);
-const isOptionSeparateFromValue = (userArg, option) => userArg === option;
+const isOptionSeparateFromValue = (mixedOption, option) => mixedOption === option;
 
-const getOptionValue = function(userArg, option, substrIndex) {
+const getOptionValue = function(mixedOptionAndValue, option, substrIndex) {
   let optionValue = 10;
-  if (isOptionValueGiven(userArg[0])) {
-    optionValue = userArg[0].substr(substrIndex);
+  if (isOptionValueGiven(mixedOptionAndValue[0])) {
+    optionValue = mixedOptionAndValue[0].substr(substrIndex);
   }
-  if (isOptionSeparateFromValue(userArg[0], option)) {
-    optionValue = userArg[1];
+  if (isOptionSeparateFromValue(mixedOptionAndValue[0], option)) {
+    optionValue = mixedOptionAndValue[1];
   }
   return optionValue;
 };
 
-const getFileReaderIndex = function(userArg, option) {
+const getFileReaderIndex = function(mixedOption, option) {
   let fileReaderIndex = 0;
-  if (isOptionValueGiven(userArg[0])) {
+  if (isOptionValueGiven(mixedOption)) {
     fileReaderIndex = 1;
   }
-  if (isOptionSeparateFromValue(userArg[0], option)) {
+  if (isOptionSeparateFromValue(mixedOption, option)) {
     fileReaderIndex = 2;
   }
   return fileReaderIndex;
@@ -42,8 +42,8 @@ const extractUserInput = function(userInput) {
   let regex = "^" + option + "[0-9]";
   regex = new RegExp(regex);
   let substrIndex = getSubstrIndex(userInput[0], regex);
-  let optionValue = getOptionValue(userInput, option, substrIndex);
-  let fileReaderIndex = getFileReaderIndex(userInput, option);
+  let optionValue = getOptionValue(userInput.slice(0,2), option, substrIndex);
+  let fileReaderIndex = getFileReaderIndex(userInput[0], option);
   filePaths = userInput.slice(fileReaderIndex);
   headPrerequisites = { filePaths, optionValue, option };
   return headPrerequisites;
