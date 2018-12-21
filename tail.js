e49b45ab8dca9ec30188d26329ef53185a7d2411
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-21 11:04:46 +0530

    Removed the usages from head.js and tail.js and added it into README.md

--- tail.js
+++ tail.js
@@ -1,19 +1,3 @@
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
-
 const { extractUserInput } = require("./src/handleUserInput.js");
 const {getContents} = require('./src/lib');
 const fs = require("fs");
